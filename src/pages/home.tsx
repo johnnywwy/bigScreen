464994f0fc91bb076d4864d0dfa4a115c01acaf3
johnnywwy <johnnywwy@gmail.com>
2022-04-21 17:16:23 +0800

    新增 header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './home.scss';
+import headerBg from '../images/img.png'
 
+console.log(headerBg);
+console.log();
 export const Home = () => {
   return (
     <div>
-      <div className='x'></div>
+      <div className='x' style={{backgroundImage:`url(${headerBg})`}}></div>
     </div>
   );
 };
