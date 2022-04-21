c139dbc2b4d33038fd8012f60aca5419dda9ada5
johnnywwy <johnnywwy@gmail.com>
2022-04-21 20:16:53 +0800

    柱形图完成

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,83 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './home.scss';
-import headerBg from '../images/img.png'
+import headerBg from '../images/img.png';
+import * as echarts from 'echarts';
 
-console.log(headerBg);
-console.log();
+const px = (n) => {
+  return n / 2420 * (window as any).pageWidth;
+};
+
 export const Home = () => {
+  const divRef = useRef(null);
+  useEffect(() => {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(divRef.current);
+    // 绘制图表
+    myChart.setOption({
+      textStyle: {
+        fontSize: px(12),
+        color: '#79839e'
+      },
+      title: {show: false},
+      legend: {show: false},
+      xAxis: {
+        data: ['兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区'],
+        axisTick: {show: false},
+        axisLine: {
+          lineStyle: {color: '#083B70'}
+        },
+        axisLabel: {
+          fontSize: px(12),
+          formatter(val) {
+            if (val.length > 2) {
+              const array= val.split('')
+              array.splice(2, 0, '\n');
+              return array.join('')
+            } else{
+              return val;
+            }
+          }
+        },
+
+      },
+      grid: {
+        x: px(40),
+        y: px(40),
+        x2: px(40),
+        y2: px(40),
+      },
+      yAxis: {
+        splitLine: {show: false},
+        axisLine: {
+          show: true,
+          lineStyle: {color: '#083B70'}
+        },
+        axisLabel: {
+          fontSize: px(12)
+        }
+      },
+      series: [
+        {
+          type: 'bar',
+          data: [10, 20, 36, 41, 15, 26, 37, 18, 29]
+        }
+      ]
+    });
+  }, []);
   return (
-    <div className='home'>
-      <header style={{backgroundImage:`url(${headerBg})`}}/>
+    <div className="home">
+      <header style={{backgroundImage: `url(${headerBg})`}}/>
       <main>
-        <section className='section1'/>
-        <section className='section2'/>
-        <section className='section3'/>
-        <section className='section4'/>
-        <section className='section5'/>
+        <section className="section1 ">
+          <div className="bordered 管辖统计">
+            <h2>案发派出所管辖统计</h2>
+            <div ref={divRef} className="chart"></div>
+          </div>
+        </section>
+        <section className="bordered section2"/>
+        <section className="bordered section3"/>
+        <section className="bordered section4"/>
+        <section className="bordered section5"/>
       </main>
     </div>
   );
